7c69b4fefbb59b04e9244aac5bbea3571f279f20
Natselane <fabien1308@gmail.com>
2020-08-01 20:49:01 +0200

    Add resize button

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const container = document.querySelector(".container");
-const reset = document.querySelector(".resetBtn");
+const resetBtn = document.querySelector(".resetBtn");
+const resizeBtn = document.querySelector(".resizeBtn");
+const randomBtn = document.querySelector(".random-colorsBtn");
+let squareDivs; 
 
 let amountOfSquares = 16;
+let color = "black";
 
 createGrid(amountOfSquares);
 
-reset.addEventListener("click", () => {
+resetBtn.addEventListener("click", () => resetGrid());
+
+resizeBtn.addEventListener("click", () => {
+    resetGrid();
     amountOfSquares = window.prompt("How many squares do you want?");
     createGrid(amountOfSquares);
+})
+
+randomBtn.addEventListener("click", () => {
+    const letters = '0123456789ABCDEF';
+    color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    paintSquares(color);
 })
 
 function createGrid(amountOfSquares) {
@@ -19,14 +35,31 @@ function createGrid(amountOfSquares) {
     container.style.gridTemplateColumns = "repeat(" + amountOfSquares + ", 1fr";
     container.style.gridTemplateRows = "repeat(" + amountOfSquares + ", 1fr";
 
-        
-
-    const squareDivs = document.querySelectorAll(".container div");
-
-    squareDivs.forEach(square => {
-        square.addEventListener("pointerover", () => {
-            square.classList.add("black");
-        })
-    })
+    paintSquares(color);
 }
 
+function paintSquares(color) {
+    squareDivs = document.querySelectorAll(".container div");
+
+    if (color == "black") {
+        squareDivs.forEach(square => {
+            square.addEventListener("pointerover", () => {
+                square.classList.add("black");
+            })
+        }) 
+    } else {
+        squareDivs.forEach(square => {
+            square.addEventListener("pointerover", () => {
+                square.style.backgroundColor = color;
+            })
+        })
+    }
+}
+
+function resetGrid() {
+    squareDivs.forEach(square => {
+        square.classList.remove("black");
+        square.style.backgroundColor = "";
+    });
+    createGrid(amountOfSquares);
+}
