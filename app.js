883d3f4084fb9c29c114de44d60fc45c943e91b4
Natselane <fabien1308@gmail.com>
2020-08-01 20:21:31 +0200

    Add style, update index.html,  update app.js

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 const container = document.querySelector(".container");
+const reset = document.querySelector(".resetBtn");
 
-let amountOfSquares = 0;
-amountOfSquares = window.prompt("How many squares?");
+let amountOfSquares = 16;
 
-container.style.gridTemplateColumns = "repeat(" + amountOfSquares + ", 10px)";
-container.style.gridTemplateRows = "repeat(" + amountOfSquares + ", 10px)";
+createGrid(amountOfSquares);
 
-for (let i = 0; i < amountOfSquares * amountOfSquares; i++) {
-    const squareDiv = document.createElement("div");
-    container.appendChild(squareDiv);
+reset.addEventListener("click", () => {
+    amountOfSquares = window.prompt("How many squares do you want?");
+    createGrid(amountOfSquares);
+})
+
+function createGrid(amountOfSquares) {
+    for (let i = 0; i < amountOfSquares * amountOfSquares; i++) {
+        let squareDiv = document.createElement("div");
+        container.appendChild(squareDiv);
+    }
+
+    container.style.gridTemplateColumns = "repeat(" + amountOfSquares + ", 1fr";
+    container.style.gridTemplateRows = "repeat(" + amountOfSquares + ", 1fr";
+
+        
+
+    const squareDivs = document.querySelectorAll(".container div");
+
+    squareDivs.forEach(square => {
+        square.addEventListener("pointerover", () => {
+            square.classList.add("black");
+        })
+    })
 }
 
-const squareDivs = document.querySelectorAll(".container div");
-
-squareDivs.forEach(square => {
-    square.addEventListener("pointerover", () => {
-        square.classList.add("red");
-    })
-})
